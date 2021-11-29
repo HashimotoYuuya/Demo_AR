b31cb88caf9b6b4e5dd82435cdf9657e07eca4e2
Hashimoto <hash419@gmail.com>
2021-11-30 08:50:07 +0900

    js

--- js/MultiplyCube.js
+++ js/MultiplyCube.js
@@ -1,12 +1,38 @@
 
+var isIncrease = true;
+var timer = null;
 
-// タッチイベント
-AFRAME.registerComponent('click-change', {
+AFRAME.registerComponent('registerevents', {
     init: function () {
-        console.log("Clicked")
-        this.el.addEventListener('click', increaseCube);
+        var marker = this.el;
+
+        // マーカー検出イベントの登録
+        marker.addEventListener('markerFound', function () {
+            var markerId = marker.id;
+            console.log('markerFound', markerId);
+            //増加/減少タイマーの開始
+            timer = setInterval(isIncrease ? increaseCube : decreaseCube, 1000);
+        });
+
+        // マーカーロストイベントの登録
+        marker.addEventListener('markerLost', function () {
+            var markerId = marker.id;
+            console.log('markerLost', markerId);
+            //増減フラグの反転
+            isIncrease = !isIncrease;
+            this.clearInterval(timer);
+        });
     }
-  });
+});
+
+//キューブ減少
+function decreaseCube() {
+    var marker = document.getElementById("marker");
+    //要素が2つ以上あれば削除
+    if (marker.childElementCount > 1) {
+        marker.removeChild(marker.lastChild);
+    }
+}
 
 //キューブ増加
 var isVertical = true;
