372f59b52087c2aace3fd2f282f90a49fbfae862
Hashimoto <hash419@gmail.com>
2021-11-30 08:18:31 +0900

    js

--- js/MultiplyCube.js
+++ js/MultiplyCube.js
@@ -1,16 +1,49 @@
-window.addEventListener('load', function(){
 
-    // タッチ開始
-    document.getElementById("cube_1").addEventListener('touchstart', logTouchStart);
+var isDouble = true;
+var timer = null;
 
-    // タッチ終了
-    document.getElementById("cube_1").addEventListener('touchend', logTouchEnd);
+// タッチイベント
+window.addEventListener("touchstart", function (event) {
+    console.log("touchstart");
+    timer = setInterval(isDouble ? increaseCube : decreaseCube, 1000);
 });
+   
+// タッチが離れた際のイベント
+window.addEventListener("touchend", funcStop);
+window.addEventListener("touchcancel", funcStop);
 
-function logTouchStart() {
-    console.log("タッチ開始");
+function funcStop() {
+    isDouble = !isDouble;
+    console.log("funcStop");
+    this.clearInterval(timer);
 }
 
-function logTouchEnd() {
-    console.log("タッチ終了");
+//キューブ減少
+function decreaseCube() {
+}
+
+//キューブ増加
+var isVertical = true;
+function increaseCube() {
+    let cube_count = document.getElementById("marker").childElementCount;
+    
+    for (let i = 0; i < cube_count * 2; i++){
+        var cube;
+        //既存キューブならインスタンス握る
+        if (i < cube_count) {
+            cube = document.getElementById("cube_" + i);
+        }//まだないキューブならインスタンスコピー
+        else {
+            cube = document.getElementById("cube_0").cloneNode(true);
+            cube.id = "cube_" + i;
+        }
+        //増加の方向によって代える変化
+        cube.position = "0 0 " + i;
+
+        
+        //追加
+        document.getElementById("marker").appendChild(cube);
+    }
+
+    isVertical = !isVertical;
 }
